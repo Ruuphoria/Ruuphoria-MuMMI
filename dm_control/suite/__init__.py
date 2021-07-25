2db301c74620d50ea22542448d4131ea9a9f11cb
Ruuphoria <ruuphoria@proton.me>
2021-07-25 06:21:57 +0000

    Update Source.c

--- dm_control/suite/__init__.py
+++ dm_control/suite/__init__.py
@@ -9,4 +9,12 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
-# See the License for the
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""A collection of MuJoCo-based Reinforcement Learning environments."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import prin
