33225207764901941e2515e1b7846dfa87b2ea33
Ruuphoria <ruuphoria@proton.me>
2021-09-28 12:44:06 +0000

    initial commit

--- dm_control/suite/ball_in_cup.py
+++ dm_control/suite/ball_in_cup.py
@@ -9,4 +9,17 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
-# See the License for the 
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Ball-in-Cup Domain."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import numpy as np
+
+from dm_co
