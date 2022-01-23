7a2c68c0c3084526629a7b385d5e0952ef7c48f8
Ruuphoria <ruuphoria@proton.me>
2022-01-23 14:50:27 +0000

    Added release plugin

--- dm_control/suite/finger.py
+++ dm_control/suite/finger.py
@@ -11,4 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==================
+# ============================================================================
+
+"""Finger Domain."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+
+from dm_control import mujoco
+from dm_control.rl import control
+from dm_control.suite import base
+from dm_contr
