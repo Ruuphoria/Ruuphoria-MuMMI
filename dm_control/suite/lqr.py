b9eecc8b15470a0a61cc8bb7a92fb5557fec018b
Ruuphoria <ruuphoria@proton.me>
2022-04-02 17:00:48 +0000

    SAK-12938

--- dm_control/suite/lqr.py
+++ dm_control/suite/lqr.py
@@ -9,4 +9,17 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
-# See the License for the specific language governing permi
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Procedurally generated LQR domain."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import os
+
+from 
