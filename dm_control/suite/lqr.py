7bbf9249d220882da19b3ff73fdb2167f55e2e74
Ruuphoria <ruuphoria@proton.me>
2022-04-22 17:02:55 +0000

    1.2.10

--- dm_control/suite/lqr.py
+++ dm_control/suite/lqr.py
@@ -248,4 +248,13 @@ class LQRLevel(base.Task):
     super(LQRLevel, self).initialize_episode(physics)
 
   def get_observation(self, physics):
-    """Returns 
+    """Returns an observation of the state."""
+    obs = collections.OrderedDict()
+    obs['position'] = physics.position()
+    obs['velocity'] = physics.velocity()
+    return obs
+
+  def get_reward(self, physics):
+    """Returns a quadratic state and control reward."""
+    position = physics.position()
+    
