71310d3a27a6a124834ffb590b97936f9b1c78c3
Ruuphoria <ruuphoria@proton.me>
2022-08-14 19:28:18 +0000

    Clean up package.json

--- dm_control/suite/wrappers/pixels.py
+++ dm_control/suite/wrappers/pixels.py
@@ -11,4 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =========================
+# ============================================================================
+
+"""Wrapper that adds pixel observations to a control environment."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+
+import dm_env
+from dm_env import specs
+
+STATE_KEY = 'state'
+
+
+class Wrapper(
